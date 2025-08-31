47dd9e4ddfbacf2d82a8e61684a6d2b66cf6b3c8
DaniJLa <klepov.bogdan@gmail.com>
2025-09-01 02:31:26 +0300

    All tasks is done

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # goit-js-hw-04
 
-"Objects"
+"04-Objects"
--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,10 @@
-function isEnoughCapacity(products, containerSize) {}
+function isEnoughCapacity(products, containerSize) {
+  let totalItems = 0;
+  for (const product in products) {
+    totalItems += products[product];
+  }
+  return totalItems <= containerSize ? true : false;
+}
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
